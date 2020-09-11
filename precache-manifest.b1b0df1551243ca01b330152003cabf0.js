b3899e2ee37f0f6d0c3f78e38754301c940dc840
chetan <cmkotwal1997@gmail.com>
2020-09-11 12:55:57 +0530

    Updates

---
+++ precache-manifest.b1b0df1551243ca01b330152003cabf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d16b93e952da65f28e09553bb552db",
+    "url": "/QuizApplication/index.html"
+  },
+  {
+    "revision": "61b19cf9a7c3910534db",
+    "url": "/QuizApplication/static/css/main.d4fea4f8.chunk.css"
+  },
+  {
+    "revision": "e0bbd7f8ffaf44849076",
+    "url": "/QuizApplication/static/js/2.45d6d40e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuizApplication/static/js/2.45d6d40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b19cf9a7c3910534db",
+    "url": "/QuizApplication/static/js/main.bb2d1536.chunk.js"
+  },
+  {
+    "revision": "68fc2dc83a2780427f4c",
+    "url": "/QuizApplication/static/js/runtime-main.b092ab62.js"
+  }
+]);
